45d31667cb1a050fe66eda53d588907cd84b87fd
Samuel R Souza <rssamuel17@gmail.com>
2023-08-24 17:15:14 -0300

    Correção de bugs na importação do Express e Cors adicionado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Servidor aberto na porta ${PORT}`));
