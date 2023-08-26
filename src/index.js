5e41bb24efcb631d096cca7ca9a9698306ea4e09
samuelsouzzza <rssamuel17@gmail.com>
2023-08-26 01:39:28 -0300

    Funcionalidade de retornar apenas um usuário por Id pronta
    Informações desnecessárias dos usuários foram apagadas

--- src/index.js
+++ src/index.js
@@ -16,22 +16,22 @@ app.get('/', (req, res) => {
 //USUÁRIOS
 const users = [
   {
+    id: 0,
     name: 'Samuel',
     surname: 'Souza',
     cpf: '52928508880',
     email: 'rssamuel17@gmail.com',
     cell: '13996976851',
-    cell_secondary: null,
-    birth: '2003-06-28',
-    address: {
-      street: 'Barra do Ribeira',
-      number: 101,
-      cep: '11900000',
-      neighborhood: 'Jardim Valeri',
-      city: 'Registro',
-      state: 'sp',
-    },
     password: 'sF44sribes#',
+  },
+  {
+    id: 1,
+    name: 'Fernanda',
+    surname: 'Lima',
+    cpf: '12345678900',
+    email: 'fenandinha@gmail.com',
+    cell: '11997986234',
+    password: 'f44souZa#s',
   },
 ];
 // ----------------------------------------------
@@ -41,13 +41,7 @@ function checkUser(req, res, next) {
     !req.body.surname ||
     !req.body.cpf ||
     !req.body.email ||
-    !req.body.cell ||
-    !req.body.birth ||
-    !req.body.address.street ||
-    !req.body.address.cep ||
-    !req.body.address.neighborhood ||
-    !req.body.address.city ||
-    !req.body.address.state
+    !req.body.cell
   ) {
     res
       .status(400)
@@ -63,18 +57,7 @@ app.get('/users', (req, res) => {
 });
 // ----------------------------------------------
 app.post('/users', checkUser, (req, res) => {
-  const {
-    name,
-    surname,
-    cpf,
-    email,
-    cell,
-    cell_secondary,
-    birth,
-    address,
-    password,
-  } = req.body;
-
+  const { name, surname, cpf, email, cell, password } = req.body;
   const newUser = {
     id: users.length + 1,
     name,
@@ -82,20 +65,13 @@ app.post('/users', checkUser, (req, res) => {
     cpf,
     email,
     cell,
-    cell_secondary,
-    birth,
-    address: {
-      street: address.street,
-      number: address.number,
-      cep: address.cep,
-      neighborhood: address.neighborhood,
-      city: address.city,
-      state: address.state,
-    },
     password,
   };
-
   users.push(newUser);
-
   return res.json('Usuário criado com sucesso');
 });
+// ----------------------------------------------
+app.get('/users/:id', (req, res) => {
+  return res.json(users[req.params.id]);
+});
+// ----------------------------------------------
