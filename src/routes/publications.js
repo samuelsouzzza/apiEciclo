6581ca7bb42510e445260ac49b8f225968b50b57
Samuel R Souza <rssamuel17@gmail.com>
2023-09-19 16:58:59 -0300

    Fotos das publicações sendo enviadas corretamente

--- src/routes/publications.js
+++ src/routes/publications.js
@@ -3,11 +3,15 @@ const multer = require('multer');
 
 const storagePublicationsImgs = multer.diskStorage({
   destination: 'uploads/publications',
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      `pub-${publications.length + 1}_${Date.now()}${file.originalname}`
-    );
+  filename: async function (req, file, cb) {
+    try {
+      const filename = `pub-${publications.length + 1}_${Date.now()}${
+        file.originalname
+      }`;
+      cb(null, filename);
+    } catch (error) {
+      cb(error);
+    }
   },
 });
 
@@ -25,8 +29,23 @@ publicationsRoutes.post(
   uploadImgs.array('publication_photos', 5),
   (req, res) => {
     const newPublication = JSON.parse(req.body.publication);
-
     newPublication.id = publications.length + 1;
+    newPublication.status = 1;
+
+    const dateNow = new Date();
+    newPublication.date_creation = `${dateNow
+      .getDate()
+      .toString()
+      .padStart(2, 0)}/${dateNow
+      .getMonth()
+      .toString()
+      .padStart(
+        2,
+        0
+      )}/${dateNow.getFullYear()} - ${dateNow.getHours()}:${dateNow.getMinutes()}`;
+
+    const photoPaths = req.files.map((file) => file.path);
+    newPublication.photos_paths = photoPaths;
 
     publications.push(newPublication);
     return res.json({ status: 201, message: 'Publicação criada com sucesso!' });
